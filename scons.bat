62a9441d2fccfade27b142b85ce6e5b6f86cc603
slipher <slipher@protonmail.com>
2026-06-26 20:21:58 -0500

    Remove scons.bat anti-Cygwin check
    On Windows 11 there is System32\bash.exe o_O
    
    This is not really relevant unless you have a sufficiently old Visual
    Studio for the vendored Scons to recognize. In all further work my
    testing was done with external Scons. But it's nice to avoid the
    obnoxious behavior of the batch file closing your cmd session!

--- scons.bat
+++ scons.bat
@@ -5,17 +5,6 @@
 
 setlocal
 
-:: If this batch file is run directly thru cygwin, we will get the wrong
-:: version of python. To avoid this, if we detect cygwin, we need to then
-:: invoke the shell script which will then re-invoke this batch file with
-:: cygwin stripped out of the path.
-:: Detect cygwin by trying to run bash.
-bash --version >NUL 2>&1
-if %ERRORLEVEL% == 0 (
-    bash "%~dp0\scons" %* || exit 1
-    goto end
-)
-
 :: Preserve a copy of the PATH (in case we need it later, mainly for cygwin).
 set PRESCONS_PATH=%PATH%
 
