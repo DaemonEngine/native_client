0690a03ba761b82ce75e727f71f908f361963221
slipher <slipher@protonmail.com>
2026-07-19 21:31:26 -0500

    Mac: clean up find_sdk.py vomit
    - Migrate to Python 3
    - Allow SDK versions greater than 10.x
    - Don't require XCode IDE to be installed

--- build/mac/find_sdk.py
+++ build/mac/find_sdk.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -22,7 +22,7 @@ from optparse import OptionParser
 
 def parse_version(version_str):
   """'10.6' => [10, 6]"""
-  return map(int, re.findall(r'(\d+)', version_str))
+  return [int(s) for s in re.findall(r'(\d+)', version_str)]
 
 
 def main():
@@ -40,6 +40,7 @@ def main():
   min_sdk_version = args[0]
 
   job = subprocess.Popen(['xcode-select', '-print-path'],
+                         text=True,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
   out, err = job.communicate()
@@ -56,7 +57,7 @@ def main():
     sdk_dir = xcode43_sdk_path
   else:
     sdk_dir = os.path.join(out.rstrip(), 'SDKs')
-  sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
+  sdks = [re.findall('^MacOSX(\d+\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
   sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
   sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
           if parse_version(s) >= parse_version(min_sdk_version)]
@@ -82,8 +83,8 @@ def main():
 
   if options.print_sdk_path:
     print(subprocess.check_output(
-        ['xcodebuild', '-version', '-sdk', 'macosx' + best_sdk,
-         'Path'], encoding='utf-8').strip())
+        ['xcrun', '--sdk', 'macosx' + best_sdk,
+         '--show-sdk-path'], encoding='utf-8').strip())
 
   return best_sdk
 
