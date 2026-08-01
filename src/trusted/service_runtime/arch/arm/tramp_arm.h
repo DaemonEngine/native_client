9a0da1f13440fb1db221474f32f2c7fdcffe8029
slipher <slipher@protonmail.com>
2026-08-01 05:54:49 -0500

    Work around memset size warning on arm

--- src/trusted/service_runtime/arch/arm/tramp_arm.h
+++ src/trusted/service_runtime/arch/arm/tramp_arm.h
@@ -7,7 +7,7 @@
 #ifndef NATIVE_CLIENT_SRC_TRUSTED_SERVICE_RUNTIME_ARCH_ARM_TRAMP_ARM_H_
 #define NATIVE_CLIENT_SRC_TRUSTED_SERVICE_RUNTIME_ARCH_ARM_TRAMP_ARM_H_
 
-extern const char NaCl_trampoline_seg_code;
+extern const char NaCl_trampoline_seg_code[];
 extern const char NaCl_trampoline_seg_end;
 extern const char NaCl_trampoline_syscall_seg_addr;
 
